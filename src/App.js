e9c8a200d0a8c96cbb46eff9ed7c36c316077f69
bader213 <143603970+bader213@users.noreply.github.com>
2024-07-14 23:46:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TicTacToe from './components/Assist/TicTacToe/TicTacToe.js';
+
+
+function App() {
+  return (
+    
+    <><TicTacToe/></>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Assist/TicTacToe/TicTacToe.js
@@ -0,0 +1,138 @@
+import React,{useState,useRef} from 'react';
+
+import'./TicTacToe.css';
+import circle_icon from '../circle.png';
+import cross_icon from '../cross.png';
+
+let data=["","","","","","","","",""];
+let boxes=document.querySelectorAll('.boxes');	
+
+function TicTacToe() {
+	
+let titleRef=useRef(null);	
+let [count,setCount]=useState(0);
+let [lock,setLock]=useState(false);
+
+
+function reset() {
+	setLock(false);
+	data=["","","","","","","","",""];
+	titleRef.current.innerHTML='Tic Tac Toe In <span>React</span>';		
+	for(let i=0;i<boxes.length;i++){
+		boxes[i].innerHTML='';
+	}
+
+	
+}
+
+
+
+
+
+
+
+function CheckWin() {
+
+	if(data[0]===data[1]&&data[1]===data[2] &&data[2]!=""){
+		won(data[2]);
+		
+	}
+	else if(data[3]===data[4]&&data[4]===data[5] &&data[5]!=""){
+		won(data[5]);
+		
+	}
+	else if(data[6]===data[7]&&data[7]===data[8] &&data[8]!=""){
+		won(data[8]);
+		
+	}
+	else if(data[0]===data[3]&&data[3]===data[6] &&data[6]!=""){
+		won(data[6]);
+	}
+	else if(data[1]===data[4]&&data[4]===data[7] &&data[7]!=""){
+		won(data[7]);
+	}
+	else if(data[2]===data[5]&&data[5]===data[8] &&data[8]!=""){
+		won(data[8]);
+	}
+	else if(data[0]===data[4]&&data[4]===data[8] &&data[8]!=""){
+		won(data[8]);
+	}
+	else if(data[2]===data[4]&&data[4]===data[6] &&data[6]!=""){
+		won(data[6]);
+	}
+
+
+
+
+
+
+}
+
+function won(winner) {
+	setLock(true);
+	if(winner==="X"){
+		titleRef.current.innerHTML=`Congratulations: <img src='${cross_icon}'/> Wins`;
+	}
+	else{
+		titleRef.current.innerHTML=`Congratulations: <img src='${circle_icon}'/> Wins`
+	}
+}
+
+
+const toggle=(e,index) =>{
+	
+	if(lock===true){
+		return 0;
+	}
+
+	if(count%2===0){
+		
+		e.target.innerHTML=`<img src='${cross_icon}'/>`;
+		
+		data[index]="X";
+		setCount(c=>c+1);
+		
+	}else{
+
+		e.target.innerHTML=`<img src='${circle_icon}'/>`;
+		
+		data[index]="O";
+		setCount(c=>c+1);
+
+	}
+	CheckWin();
+
+
+}
+
+
+return(
+	<div className="container">
+		<h1 className="title" ref={titleRef}>Tic Tac Toe Game In <span>React</span> </h1>
+
+	    <div className="board">
+	      	<div className="row1">
+	      		<div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
+	      		<div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
+	      		<div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
+	      	</div>
+	      	<div className="row2">
+	      		<div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
+	      		<div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
+	      		<div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
+	      	</div>
+	      	<div className="row3">
+	      		<div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
+	      		<div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
+	      		<div className="boxes" onClick={(e)=>{toggle(e,8)}}></div>
+	      	</div>
+	    </div>		
+	    <button className="reset" onClick={()=>reset()}>Reset</button>
+
+
+	</div>
+);
+
+
+}
+export default TicTacToe;
